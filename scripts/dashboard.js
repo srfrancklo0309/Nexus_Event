7047a8aaa14c5d5440b3d4a9f93810a3031538db
marianarestrepo26 <marianartpo08@gmail.com>
2025-07-07 17:00:01 -0500

    Dashboard, suscriptionAPI y contactAPI

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,96 +1,67 @@
-document.addEventListener("DOMContentLoaded", () => {
+import { getEvents } from "../api/eventAPI.js";
+import { getContacts } from "../api/contactAPI.js";
+import { getSuscriptions } from "../api/suscriptionAPI.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
   
-  
-  fetch("../assets/db/db.json")
-    .then(response => response.json())
-    .then(data => {
-      const activeEvents = data.events;
-      const activeCount = activeEvents.filter(event => event.status === "active").length;
-      if (activeCount === 0) {
-        document.getElementById("canceled-events").innerHTML = "No events available";
-        return;
-      }
+  async function loadEventsCounter() {
+    try {
+      const { data: events } = await getEvents();
+
+      const activeCount = events.filter(event => event.status === "active").length;
+      const inactiveCount = events.filter(event => event.status === "inactive").length;
+      const canceledCount = events.filter(event => event.status === "canceled").length;
+
       const activeEventsDiv = document.getElementById("active-events");
       activeEventsDiv.innerHTML = `
         <p class="stat-label">Active Events</p>
         <p class="stat-value">${activeCount}</p>`;
-    })
-    .catch(error => {
-      document.getElementById("active-events").innerHTML = "Error loading events";
-      console.error(error);
-    });
-
-    fetch("../assets/db/db.json")
-    .then(response => response.json())
-    .then(data => {
-      const inactiveEvents = data.events;
-      const inactiveCount = inactiveEvents.filter(event => event.status === "inactive").length;
-      if (inactiveCount === 0) {
-        document.getElementById("canceled-events").innerHTML = "No events available";
-        return;
-      }
+      
       const inactiveEventsDiv = document.getElementById("inactive-events");
       inactiveEventsDiv.innerHTML = `
         <p class="stat-label">Inactive Events</p>
         <p class="stat-value">${inactiveCount}</p>`;
-    })
-    .catch(error => {
-      document.getElementById("active-events").innerHTML = "Error loading events";
-      console.error(error);
-    });
 
-    fetch("../assets/db/db.json")
-    .then(response => response.json())
-    .then(data => {
-      const canceledEvents = data.events;
-      const canceledCount = canceledEvents.filter(event => event.status === "canceled").length;
-      if (canceledCount === 0) {
-        document.getElementById("canceled-events").innerHTML = "No events available";
-        return;
-      }
       const canceledEventsDiv = document.getElementById("canceled-events");
       canceledEventsDiv.innerHTML = `
         <p class="stat-label">Canceled Events</p>
         <p class="stat-value">${canceledCount}</p>`;
-    })
-    .catch(error => {
-      document.getElementById("active-events").innerHTML = "Error loading events";
+    } catch (error) {
       console.error(error);
-    });
+    }
+  }
 
-        fetch("../assets/db/db.json")
-    .then(response => response.json())
-    .then(data => {
-      const suscriptionsEmails = data.suscriptions;
-      const suscriptionsCounter = suscriptionsEmails.length;
-      if (suscriptionsCounter === 0) {
-        document.getElementById("registered-emails").innerHTML = "No subscriptions available";
-        return;
-      }
-      const registeredEmailsDiv = document.getElementById("registered-emails");
-      registeredEmailsDiv.innerHTML = `
-        <p class="stat-value">${suscriptionsCounter}</p>`;
-    })
-    .catch(error => {
-      document.getElementById("active-events").innerHTML = "Error loading suscriptions";
-      console.error(error);
-    });
+  async function loadContactsCounter() {
+    try {
+      const {data : contacts} = await getContacts();
+      
+      const contactsCounter = contacts.length;
 
-    fetch("../assets/db/db.json")
-    .then(response => response.json())
-    .then(data => {
-      const contactMessages = data.contacts;
-      const messagesCounter = contactMessages.length;
-      if (messagesCounter === 0) {
-        document.getElementById("contact-messages").innerHTML = "No messages available";
-        return;
-      }
-      const registeredEmailsDiv = document.getElementById("contact-messages");
-      registeredEmailsDiv.innerHTML = `
-        <p class="stat-value">${messagesCounter}</p>`;
-    })
-    .catch(error => {
-      document.getElementById("active-events").innerHTML = "Error loading messages";
-      console.error(error);
-    });
+      const contactMessages = document.getElementById("contact-messages");
+      contactMessages.innerHTML = `
+        <p class="stat-label recent-activity-cards">Messages</p>
+        <p class="stat-value">${contactsCounter}</p>`;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function loadSuscriptionsCounter() {
+  try {
+    const {data : suscriptions} = await getSuscriptions();
+    
+    const suscriptionsCounter = suscriptions.length;
+
+    const registeredEmails = document.getElementById("registered-emails");
+    registeredEmails.innerHTML = `
+      <p class="stat-label recent-activity-cards">Suscriptions</p>
+      <p class="stat-value">${suscriptionsCounter}</p>`;
+  } catch (error) {
+    console.log(error);
+  }
+  }
+
+  await loadSuscriptionsCounter();
+  await loadEventsCounter();
+  await loadContactsCounter();
 });
